5299297f501ead6c9123c72d2d9b92b92e5a6adb
farhan <farhan.khan@edly.io>
2022-03-16 19:32:42 +0500

    Add requirements.txt

--- zoom_utils/zoom_report.py
+++ zoom_utils/zoom_report.py
@@ -10,18 +10,24 @@ class ZoomMeetingReport:
         self.meeting_name = meeting_name
 
     def get_report(self):
-        # meeting_id = '88177466375'
-        meeting_id = '86345689524'
+        meeting_id = '88177466375'
+        # meeting_id = '86345689524'
         zoom_client = ZoomAPIClient(self.zoom_admin_account)
         participants = []
         past_instances = zoom_client.get_meeting_instances(meeting_id).get('meetings', [])
         for instance in past_instances:
-            uuid = get_double_encoded_uuid(instance['uuid'])
+            # uuid = get_double_encoded_uuid(instance['uuid'])
+            uuid = instance['uuid']
             zoom_participants = zoom_client.get_participant_report(uuid)
             participants += zoom_participants
 
         participants_duration = {}
+        index = 0
         for participant in participants or []:
-            user_email = participant.get('user_email')
-            participants_duration[user_email] += participant.get('duration', 0)
+            user_email = participant.get('user_email') + '_' + str(index)
+            if user_email in participants_duration.keys():
+                participants_duration[user_email] += participant.get('duration', 0)
+            else:
+                participants_duration[user_email] = participant.get('duration', 0)
+            index += 1
         return participants_duration
